8be412563b93b40035dee65976b9fb2cbc155c52
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2021-09-26 22:46:52 +0700

    Pull

---
+++ bookstore.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore | Cửa hàng bán sách online</title>
+    <link rel="stylesheet"  type = "text/css" href="bookstore.css">
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <img src="image/icon/logo2.png" alt="logo" class="logo">
+            <nav>
+                <ul>
+                    <li><a href="">Trang chủ</a></li>
+                    <li><a href="">Hỗ trợ</a></li>
+                    <li><a href="">Thông báo</a></li>
+                    <li><a href="">Bộ sách mới</a></li>
+                    <li><a href="login.php">Đăng nhập</a></li>
+                </ul>
+            </nav>
+            <span class = "cart">
+                <img src="image/icon/shopping_cart_50px.png" alt="Giỏ hàng">
+                <p>Giỏ hàng</p>
+            </span>
+        </div>
+        <div class = "hotsale">
+            <a href="" class = "click">Hot Sale tháng 9 &#x1F525</a>
+            <h1>Nghệ Thuật Từ Chối <br> Cách Nói Không Mà <br> Vẫn Có Được Đồng Thuận</h1>
+            <p>Từ chối một ai đó bằng việc nói “KHÔNG” ngay lập tức thì thật sỗ sàng.<br>Chính vì vậy mà cách từ chối khéo.... <a class = "more" href="">Tìm hiểu thêm</a></p>
+            <div class="arrow-icons">
+                <img src="image/icon/left_50px.png" alt="">
+                <img src="image/icon/right_50px.png" alt="">
+            </div>
+        </div>
+        <img src="image/unnamed.png" class = "sanpham">
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type = "text/css" href="login.css">
+</head>
+<body>
+    <div class = "login">
+        <form action="#", method="POST">
+            <h2>Đăng nhập</h2><br>
+            <p>Tài khoản</p><br>
+            <input type="text" placeholder="Nhập tài khoản"><br>
+            <p>Mật khẩu</p><br>
+            <input type="password" placeholder="Nhập mật khẩu">
+            <br>
+            <div class = "check">
+                <input type="checkbox"> Nhớ mật khẩu
+            </div>
+            <br>
+            <button>Đăng nhập</button><br>
+            <a href="#"> Quên mật khẩu</a>
+        </form>
+    </div>
+</body>
+</html>
